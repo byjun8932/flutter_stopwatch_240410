4e085841670583f3cfa4ba406d5a8aa09d591fc7
byjun8932 <byjun8932@gmail.com>
2024-04-10 21:01:20 +0900

    feat: play, pause UI

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -10,6 +10,30 @@ class StopWatchScreen extends StatefulWidget {
 class _StopWatchScreenState extends State<StopWatchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스탑워치'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.lime,
+            onPressed: () {},
+            child: const Icon(Icons.refresh),
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.pink,
+            onPressed: () {},
+            child: const Icon(Icons.play_arrow),
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.lightBlue,
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
   }
 }
