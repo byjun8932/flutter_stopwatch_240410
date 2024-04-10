f7a4cd05f21a01a36d062c54e7a60cfff6092774
byjun8932 <byjun8932@gmail.com>
2024-04-10 21:34:23 +0900

    feat: Rap-time at ListView

--- lib/stop_watch_screen.dart
+++ lib/stop_watch_screen.dart
@@ -11,35 +11,67 @@ class _StopWatchScreenState extends State<StopWatchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('스탑워치'),
-      ),
-      body: Column(
-        children: [
-          Spacer(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              FloatingActionButton(
-                backgroundColor: Colors.lime,
-                onPressed: () {},
-                child: const Icon(Icons.refresh),
+        appBar: AppBar(
+          title: const Text('스탑워치'),
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 30,),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 50),
+                ),
+                Text(
+                  '00',
+                )
+              ],
+            ),
+            SizedBox(
+              width: 100,
+              height: 200,
+              child: ListView(
+                children: [
+                   Center(child: Text('111')),
+                  Text('111'),
+                  Text('111'),
+                  Text('111'),
+                  Text('111'),
+                  Text('111'),
+                  Text('111'),
+                  Text('111'),
+                  Text('111'),
+                ],
               ),
-              FloatingActionButton(
-                backgroundColor: Colors.pink,
-                onPressed: () {},
-                child: const Icon(Icons.play_arrow),
-              ),
-              FloatingActionButton(
-                backgroundColor: Colors.lightBlue,
-                onPressed: () {},
-                child: const Icon(Icons.add),
-              ),
-            ],
-          ),
-          SizedBox(height: 30,)
-        ],
-      )
-    );
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  backgroundColor: Colors.lime,
+                  onPressed: () {},
+                  child: const Icon(Icons.refresh),
+                ),
+                FloatingActionButton(
+                  backgroundColor: Colors.pink,
+                  onPressed: () {},
+                  child: const Icon(Icons.play_arrow),
+                ),
+                FloatingActionButton(
+                  backgroundColor: Colors.lightBlue,
+                  onPressed: () {},
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            )
+          ],
+        ));
   }
 }
